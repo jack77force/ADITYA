b73683bd9309a48c62593a19e92cedc902fe529e
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-24 13:11:13 +0530

    BRANDED

--- AnonX/utils/logger.py
+++ AnonX/utils/logger.py
@@ -1,31 +1,31 @@
-from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
-from AnonX import app
-from AnonX.utils.database import is_on_off
+from pyrogram.enums import ParseMode
+
+from AnonXMusic import app
+from AnonXMusic.utils.database import is_on_off
+from config import LOGGER_ID
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(LOG):
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+    if await is_on_off(2):
         logger_text = f"""
-**{MUSIC_BOT_NAME} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
+<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-**ᴄʜᴀᴛ:** {message.chat.title} [`{message.chat.id}`]
-**ᴜsᴇʀ:** {message.from_user.mention}
-**ᴜsᴇʀɴᴀᴍᴇ:** @{message.from_user.username}
-**ɪᴅ:** `{message.from_user.id}`
-**ᴄʜᴀᴛ ʟɪɴᴋ:** {chatusername}
+<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
+<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
 
-**sᴇᴀʀᴄʜᴇᴅ ғᴏʀ:** {message.text}
+<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
+<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
-**sᴛʀᴇᴀᴍ ᴛʏᴩᴇ:** {streamtype}"""
-        if message.chat.id != LOG_GROUP_ID:
+<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+        if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
-                    LOG_GROUP_ID,
+                    chat_id=LOGGER_ID,
                     text=logger_text,
+                    parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
             except:
