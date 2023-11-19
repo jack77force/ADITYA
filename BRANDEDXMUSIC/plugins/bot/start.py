912a0293629462a29eca3bc7e44c114c9a9602a0
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 16:48:42 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/bot/start.py
+++ BRANDEDXMUSIC/plugins/bot/start.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import _boot_
-from AnonXMusic.plugins.sudo.sudoers import sudoers_list
-from AnonXMusic.utils.database import (
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.misc import _boot_
+from BRANDEDXMUSIC.plugins.sudo.sudoers import sudoers_list
+from BRANDEDXMUSIC.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +17,9 @@ from AnonXMusic.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from AnonXMusic.utils.decorators.language import LanguageStart
-from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
+from BRANDEDXMUSIC.utils.decorators.language import LanguageStart
+from BRANDEDXMUSIC.utils.formatters import get_readable_time
+from BRANDEDXMUSIC.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
