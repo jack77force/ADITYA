165e47a96600715e8864f52ca55f6ae77d63df92
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 18:19:26 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/sudo/logger.py
+++ BRANDEDXMUSIC/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.misc import SUDOERS
+from BRANDEDXMUSIC.utils.database import add_off, add_on
+from BRANDEDXMUSIC.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
