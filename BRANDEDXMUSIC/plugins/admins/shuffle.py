d19c2000288409899200fc37c6f1e46ac8138ee9
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 16:27:06 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/admins/shuffle.py
+++ BRANDEDXMUSIC/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.misc import db
+from BRANDEDXMUSIC.utils.decorators import AdminRightsCheck
+from BRANDEDXMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
