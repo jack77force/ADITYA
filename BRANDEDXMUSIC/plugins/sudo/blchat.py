3c84fdf93ef7a753355b85e5981a53a32a932558
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 18:10:10 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/sudo/blchat.py
+++ BRANDEDXMUSIC/plugins/sudo/blchat.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.misc import SUDOERS
+from BRANDEDXMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
 from AnonXMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
