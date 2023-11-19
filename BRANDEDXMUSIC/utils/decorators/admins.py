1eb2991390f55bba9b21b2da144210aa1c39d49c
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 19:25:12 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/utils/decorators/admins.py
+++ BRANDEDXMUSIC/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.misc import SUDOERS, db
+from BRANDEDXMUSIC.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
