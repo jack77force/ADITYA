6b3a0470f67cab5a2f29944031696c8556a3b0ff
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 18:51:32 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/tools/speedtest.py
+++ BRANDEDXMUSIC/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.decorators.language import language
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.misc import SUDOERS
+from BRANDEDXMUSIC.utils.decorators.language import language
 
 
 def testspeed(m, _):
