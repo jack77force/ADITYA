eba6f8a539f3811ba989340acaec100c696ee3f9
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 16:34:49 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/admins/stop.py
+++ BRANDEDXMUSIC/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.core.call import Anony
+from BRANDEDXMUSIC.utils.database import set_loop
+from BRANDEDXMUSIC.utils.decorators import AdminRightsCheck
+from BRANDEDXMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
