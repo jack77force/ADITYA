ffee6e194db2f513310f583d20f3e3c009339072
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 20:47:50 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/__init__.py
+++ BRANDEDXMUSIC/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from BRANDEDXMUSIC.core.bot import Anony
+from BRANDEDXMUSIC.core.dir import dirr
+from BRANDEDXMUSIC.core.git import git
+from BRANDEDXMUSIC.core.userbot import Userbot
+from BRANDEDXMUSIC.misc import dbb, heroku
 
 from .logging import LOGGER
 
