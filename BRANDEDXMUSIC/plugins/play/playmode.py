e3f706a8a0309c3c4327b7ff68168ec2cc71453a
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 17:10:04 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/play/playmode.py
+++ BRANDEDXMUSIC/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from AnonXMusic.utils.decorators import language
-from AnonXMusic.utils.inline.settings import playmode_users_markup
+from BRANDEDXMUSIC import app
+from BRANDEDXMUSIC.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from BRANDEDXMUSIC.utils.decorators import language
+from BRANDEDXMUSIC.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
