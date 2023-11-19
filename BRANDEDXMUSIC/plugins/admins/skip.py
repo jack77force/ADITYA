f22745f65349e0c5add51bec6d90f68be91a0990
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-19 16:30:12 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/admins/skip.py
+++ BRANDEDXMUSIC/plugins/admins/skip.py
@@ -2,14 +2,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup, stream_markup
-from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
+from BRANDEDXMUSIC import YouTube, app
+from BRANDEDXMUSIC.core.call import Anony
+from BRANDEDXMUSIC.misc import db
+from BRANDEDXMUSIC.utils.database import get_loop
+from BRANDEDXMUSIC.utils.decorators import AdminRightsCheck
+from BRANDEDXMUSIC.utils.inline import close_markup, stream_markup
+from BRANDEDXMUSIC.utils.stream.autoclear import auto_clean
+from BRANDEDXMUSIC.utils.thumbnails import get_thumb
 from config import BANNED_USERS
 
 
