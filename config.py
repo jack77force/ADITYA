b7f8c0d8e3c179978881f0240fc4a6aca91f834f
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-11-20 23:55:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", 
-    "https://github.com/WCGKING/BRANDEDXMUSIC"
+    "https://github.com/KrishnaxMusic/BRANDEDXMUSIC"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
