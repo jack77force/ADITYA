f6182110f17de30f2782eb74084dbe06deb0e9f5
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-20 09:40:05 +0530

    BRANDEDXMUSIC

--- BRANDEDXMUSIC/plugins/sudo/autoend.py
+++ BRANDEDXMUSIC/plugins/sudo/autoend.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 
 from BRANDEDXMUSIC import app
 from BRANDEDXMUSIC.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off, autoend_on
+from BRANDEDXMUSIC.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
